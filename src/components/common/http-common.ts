d4a46e47521aa165b28b6a25be6deb1675ef7435
87Unlimited <94755505+87Unlimited@users.noreply.github.com>
2024-06-08 08:48:33 +0000

    Initial commit

---
+++ src/components/common/http-common.ts
@@ -0,0 +1,7 @@
+export const api = {
+  uri:"https://bdeb2bcb-4ed9-4e23-a09e-d41cfbb4ec6a-00-19iplr3aoii9e.sisko.replit.dev:3000/api/v1"
+}
+//https://feec368b-f2cd-4c78-ba6d-d66f03597562-00-2xj5l7c94qwox.sisko.repl.co:3000/
+//5180bb11-29ac-4f58-97b1-2b025e251db8-00-3415vnygwmulx.sisko.replit.dev/
+
+//90b48509-8be0-43fd-aa07-b3855da4015a-00-2dafu0mps44fp.pike.replit.dev/
